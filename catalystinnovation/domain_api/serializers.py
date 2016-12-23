13794c4ccadca730d8b6a85bf7e9900a58ab76ad
Travis Holton <travis@catalyst.net.nz>
2016-12-23 15:01:36 +1300

    superclass modelserializer to get fields

--- catalystinnovation/domain_api/serializers.py
+++ catalystinnovation/domain_api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from domain_api.models import Identity, PersonalDetail
 
-class IdentitySerializer(serializers.Serializer):
+class IdentitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Identity
         fields = ('id', 'first_name', 'surname', 'middle_name', 'username',)
