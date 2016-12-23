31d549b617167ae1359ba27e526d63b10db72bc6
Travis Holton <travis@catalyst.net.nz>
2016-12-24 09:55:08 +1300

    Add support for schema view

--- catalystinnovation/catalystinnovation/urls.py
+++ catalystinnovation/catalystinnovation/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title='Domain API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('domain_api.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url('^schema/$', schema_view),
 ]
