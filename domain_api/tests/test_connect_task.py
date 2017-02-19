b152f65e515b7d6eb71549bca88ec9d7f1c064fd
Travis Holton <travis@catalyst.net.nz>
2017-02-20 08:37:25 +1300

    Begin testing some of celery run tasks

---
+++ domain_api/tests/test_connect_task.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from ..tasks import connect_domain
+
+
+class TestConnectTask(TestCase):
+
+    """Docstring for TestConnectTask. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
