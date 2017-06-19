9d0608c8568c25ced70ae6c32648edc31b68efa1
Travis Holton <travis@catalyst.net.nz>
2017-06-20 08:00:03 +1200

    Test jsonvalidation for nameservers

--- domain_api/tests/test_json_validation.py
+++ domain_api/tests/test_json_validation.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import jsonschema
 from .. import schemas
+from rest_framework.exceptions import ValidationError
 
 
 class TestNameserverValidation(TestCase):
@@ -9,7 +10,22 @@ class TestNameserverValidation(TestCase):
     Test validation of nameserver data structures.
     """
 
-    def test_ip_addr_validation(self):
+    def validator(self, data, schema_field):
+        """
+        Perform validation and return a boolean indicating valid or not valid.
+
+        :data: Data to validate
+        :schema_field: schema to validate against
+        :returns: Boolean
+
+        """
+        try:
+            jsonschema.validate(data, schema_field)
+        except ValidationError:
+            return False
+        return True
+
+    def test_mixed_ip_addr_validation(self):
         """
         Test validatio of complete nameserver field
         :returns: TODO
@@ -32,5 +48,57 @@ class TestNameserverValidation(TestCase):
                 },
             ]
         }
-        jsonschema.validate(nameserver, schemas.ip_field)
-        self.assertTrue(True)
+        result = self.validator(nameserver, schemas.ip_field)
+        self.assertTrue(result, "Valid nameserver")
+
+    def test_dict_field_ip_addr_validation(self):
+        """
+        Test validatio of complete nameserver field
+        :returns: TODO
+
+        """
+        nameserver = {
+            "name": "ns1.nameserver.com",
+            "addr": [
+                {
+                    "ip": "22.33.44.55",
+                    "type": "v4"
+                },
+                {
+                    "ip": "22.33.44.55",
+                },
+                {
+                    "ip": "::FE::0",
+                    "type": "v6"
+                },
+            ]
+        }
+        result = self.validator(nameserver, schemas.ip_field)
+        self.assertTrue(result, "Valid nameserver")
+
+    def test_string_field_ip_addr_validation(self):
+        """
+        Test validatio of complete nameserver field
+        :returns: TODO
+
+        """
+        nameserver = {
+            "name": "ns1.nameserver.com",
+            "addr": [
+                "11.22.33.44", "22.33.44"
+            ]
+        }
+        result = self.validator(nameserver, schemas.ip_field)
+        self.assertTrue(result, "Valid nameserver")
+
+    def test_name_only_validation(self):
+        """
+        Test validatio of complete nameserver field
+        :returns: TODO
+
+        """
+        nameserver = {
+            "name": "ns1.nameserver.com"
+        }
+        result = self.validator(nameserver, schemas.ip_field)
+        self.assertTrue(result, "Valid nameserver")
